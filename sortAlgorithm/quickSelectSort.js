48ff74813943818d1c22656d6112e66c1dacf3cd
spursy <582214816@qq.com>
2017-04-27 16:29:08 +0800

    add comment for quick sort function.

--- sortAlgorithm/quickSelectSort.js
+++ sortAlgorithm/quickSelectSort.js
@@ -8,18 +8,21 @@ function quickSort(arr, left, right) {
         var i = left;
         var j = right;
         while(i < j) {
+            // Get bigger one than temp from end
             while (i < j && temp <= arr[j]) {
                 j --;
             }
             if (i == j) break;
             arr[i++] = arr[j];
+
+            // Get smaller one than temp from begin
             while (i < j && temp > arr[i]) {
                 i ++;     
             }
             if (i == j) break;
             arr[j--] = arr[i];
         }
-
+        
         if (i == j)
             arr[i] = temp;      
 
