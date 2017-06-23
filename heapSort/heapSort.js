3c43b576bd1942837e02b2a809df3f4ad93f1e1f
spursy <582214816@qq.com>
2017-06-23 08:19:47 +0800

    add heap sort function.

---
+++ heapSort/heapSort.js
@@ -0,0 +1,12 @@
+exports.insert = function(data) {
+    var arrr = new Array();
+    arr.push(data);
+    shiftUp(arr);
+    return arr;
+}
+
+function shiftUp(arr) {
+    var k = arr.length;
+}
+
+
